bf00f4a37c18c6cc09ae4038391d8b3744436072
Davi Maia <Davirtmaia@gmail.com>
2024-08-15 23:15:03 -0300

    feat: create BadRequestException, BadRequestExceptionsDetails and RestExceptionHandler

---
+++ src/main/java/Project/BookStore/Exceptions/BadRequestException.java
@@ -0,0 +1,10 @@
+package Project.BookStore.Exceptions;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus
+public class BadRequestException extends RuntimeException{
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/Project/BookStore/Exceptions/BadRequestExceptionDetails.java
@@ -0,0 +1,17 @@
+package Project.BookStore.Exceptions;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class BadRequestExceptionDetails {
+    private String title;
+    private String details;
+    private String developerMessage;
+    private int status;
+    private LocalDateTime timestamp;
+
+}
